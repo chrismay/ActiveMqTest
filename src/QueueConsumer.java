eb43fb5d2051f8823948e48cbef03236cecca270
Chris May <chris.may@warwick.ac.uk>
2009-11-01 15:30:21 +0000

    working now

--- src/QueueConsumer.java
+++ src/QueueConsumer.java
@@ -1,4 +1,31 @@
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
-public class QueueConsumer {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
+public class QueueConsumer  implements MessageListener {
+
+	    public void onMessage(Message message) {
+	        if (message instanceof TextMessage) {
+	            try {
+	                System.out.println("got messsage: " +((TextMessage) message).getText());
+	            }
+	            catch (JMSException ex) {
+	                throw new RuntimeException(ex);
+	            }
+	        }
+	        else {
+	            throw new IllegalArgumentException("Message must be of type TextMessage");
+	        }
+	    }
+	
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				new String[] { "consumer-context.xml" });
+	}
 }
--- src/QueueProducer.java
+++ src/QueueProducer.java
@@ -1,12 +1,40 @@
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Queue;
+import javax.jms.Session;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 
 public class QueueProducer {
-public static void main(String[] args) {
-	
-	ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[] {"context.xml"});
-	for (int i = 0; i<= 100; i++) {
-		QueuedWorkItem item = new QueuedWorkItem();
+
+	private JmsTemplate template;
+	private Queue q;
+
+	public QueueProducer(JmsTemplate template, Queue queue) {
+		this.template = template;
+		this.q = queue;
 	}
-}}
+
+	public void queueStuff() {
+		for (int i = 0; i <= 100; i++) {
+			final int j = i;
+			System.out.println("queueing message " +i);
+			template.send(this.q, new MessageCreator() {
+				public Message createMessage(Session arg0) throws JMSException {
+					return arg0.createTextMessage("hello world: message #" + j);
+				}
+			});
+		}
+	}
+
+	public static void main(String[] args) {
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				new String[] { "context.xml" });
+		QueueProducer producer = (QueueProducer) ctx.getBean("producer");
+		producer.queueStuff();
+	}
+}
